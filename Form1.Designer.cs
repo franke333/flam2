45e8945d585d4efa3039b69872b18a6f2135780a
Srb František <47794968+franke333@users.noreply.github.com>
2020-08-22 11:24:15 +0200

    connected interface

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,21 +30,23 @@
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBoxWidth = new System.Windows.Forms.TextBox();
+            this.textBoxHeight = new System.Windows.Forms.TextBox();
+            this.textBoxCenterY = new System.Windows.Forms.TextBox();
+            this.textBoxCenterX = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBoxOversampling = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBoxScale = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox13 = new System.Windows.Forms.TextBox();
+            this.textBoxPalette = new System.Windows.Forms.TextBox();
             this.label7 = new System.Windows.Forms.Label();
-            this.textBox15 = new System.Windows.Forms.TextBox();
+            this.textBoxQuality = new System.Windows.Forms.TextBox();
             this.label8 = new System.Windows.Forms.Label();
-            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBoxFuncs = new System.Windows.Forms.TextBox();
             this.buttonGenerate = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxFlames = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -53,9 +55,10 @@
             this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.pictureBox1.Location = new System.Drawing.Point(274, 2);
+            this.pictureBox1.Location = new System.Drawing.Point(411, 3);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(938, 631);
+            this.pictureBox1.Size = new System.Drawing.Size(1407, 971);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
@@ -63,160 +66,213 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label1.Location = new System.Drawing.Point(7, 11);
+            this.label1.Location = new System.Drawing.Point(10, 17);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(85, 20);
+            this.label1.Size = new System.Drawing.Size(128, 29);
             this.label1.TabIndex = 1;
             this.label1.Text = "Resolution";
             // 
-            // textBox1
+            // textBoxWidth
             // 
-            this.textBox1.Location = new System.Drawing.Point(115, 13);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(77, 20);
-            this.textBox1.TabIndex = 2;
+            this.textBoxWidth.Location = new System.Drawing.Point(172, 20);
+            this.textBoxWidth.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBoxWidth.Name = "textBoxWidth";
+            this.textBoxWidth.Size = new System.Drawing.Size(114, 26);
+            this.textBoxWidth.TabIndex = 2;
+            this.textBoxWidth.Text = "300";
             // 
-            // textBox2
+            // textBoxHeight
             // 
-            this.textBox2.Location = new System.Drawing.Point(198, 13);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(72, 20);
-            this.textBox2.TabIndex = 3;
+            this.textBoxHeight.Location = new System.Drawing.Point(297, 20);
+            this.textBoxHeight.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBoxHeight.Name = "textBoxHeight";
+            this.textBoxHeight.Size = new System.Drawing.Size(106, 26);
+            this.textBoxHeight.TabIndex = 3;
+            this.textBoxHeight.Text = "300";
+            this.textBoxHeight.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
-            // textBox3
+            // textBoxCenterY
             // 
-            this.textBox3.Location = new System.Drawing.Point(198, 43);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(72, 20);
-            this.textBox3.TabIndex = 6;
+            this.textBoxCenterY.Location = new System.Drawing.Point(297, 66);
+            this.textBoxCenterY.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBoxCenterY.Name = "textBoxCenterY";
+            this.textBoxCenterY.Size = new System.Drawing.Size(106, 26);
+            this.textBoxCenterY.TabIndex = 6;
+            this.textBoxCenterY.Text = "0";
+            this.textBoxCenterY.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
-            // textBox4
+            // textBoxCenterX
             // 
-            this.textBox4.Location = new System.Drawing.Point(115, 43);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(77, 20);
-            this.textBox4.TabIndex = 5;
+            this.textBoxCenterX.Location = new System.Drawing.Point(172, 66);
+            this.textBoxCenterX.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBoxCenterX.Name = "textBoxCenterX";
+            this.textBoxCenterX.Size = new System.Drawing.Size(114, 26);
+            this.textBoxCenterX.TabIndex = 5;
+            this.textBoxCenterX.Text = "0";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label2.Location = new System.Drawing.Point(7, 41);
+            this.label2.Location = new System.Drawing.Point(10, 63);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(96, 20);
+            this.label2.Size = new System.Drawing.Size(145, 29);
             this.label2.TabIndex = 4;
             this.label2.Text = "Center [-1,1]";
             // 
-            // textBox5
+            // textBoxOversampling
             // 
-            this.textBox5.Location = new System.Drawing.Point(198, 102);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(72, 20);
-            this.textBox5.TabIndex = 12;
+            this.textBoxOversampling.Location = new System.Drawing.Point(297, 157);
+            this.textBoxOversampling.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBoxOversampling.Name = "textBoxOversampling";
+            this.textBoxOversampling.Size = new System.Drawing.Size(106, 26);
+            this.textBoxOversampling.TabIndex = 12;
+            this.textBoxOversampling.Text = "2";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label3.Location = new System.Drawing.Point(7, 100);
+            this.label3.Location = new System.Drawing.Point(10, 154);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(156, 20);
+            this.label3.Size = new System.Drawing.Size(238, 29);
             this.label3.TabIndex = 10;
             this.label3.Text = "Oversampling (1=off)";
             // 
-            // textBox7
+            // textBoxScale
             // 
-            this.textBox7.Location = new System.Drawing.Point(198, 72);
-            this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(72, 20);
-            this.textBox7.TabIndex = 9;
+            this.textBoxScale.Location = new System.Drawing.Point(297, 111);
+            this.textBoxScale.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBoxScale.Name = "textBoxScale";
+            this.textBoxScale.Size = new System.Drawing.Size(106, 26);
+            this.textBoxScale.TabIndex = 9;
+            this.textBoxScale.Text = "100";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label4.Location = new System.Drawing.Point(7, 70);
+            this.label4.Location = new System.Drawing.Point(10, 108);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(159, 20);
+            this.label4.Size = new System.Drawing.Size(245, 29);
             this.label4.TabIndex = 7;
             this.label4.Text = "Scale (pixels per unit)";
             // 
-            // textBox13
+            // textBoxPalette
             // 
-            this.textBox13.Location = new System.Drawing.Point(198, 161);
-            this.textBox13.Name = "textBox13";
-            this.textBox13.Size = new System.Drawing.Size(72, 20);
-            this.textBox13.TabIndex = 18;
+            this.textBoxPalette.Location = new System.Drawing.Point(297, 248);
+            this.textBoxPalette.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBoxPalette.Name = "textBoxPalette";
+            this.textBoxPalette.Size = new System.Drawing.Size(106, 26);
+            this.textBoxPalette.TabIndex = 18;
+            this.textBoxPalette.Text = "0";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label7.Location = new System.Drawing.Point(7, 159);
+            this.label7.Location = new System.Drawing.Point(10, 245);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(59, 20);
+            this.label7.Size = new System.Drawing.Size(88, 29);
             this.label7.TabIndex = 16;
             this.label7.Text = "Palette";
             // 
-            // textBox15
+            // textBoxQuality
             // 
-            this.textBox15.Location = new System.Drawing.Point(198, 131);
-            this.textBox15.Name = "textBox15";
-            this.textBox15.Size = new System.Drawing.Size(72, 20);
-            this.textBox15.TabIndex = 15;
+            this.textBoxQuality.Location = new System.Drawing.Point(297, 202);
+            this.textBoxQuality.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBoxQuality.Name = "textBoxQuality";
+            this.textBoxQuality.Size = new System.Drawing.Size(106, 26);
+            this.textBoxQuality.TabIndex = 15;
+            this.textBoxQuality.Text = "5";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label8.Location = new System.Drawing.Point(7, 129);
+            this.label8.Location = new System.Drawing.Point(10, 198);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(57, 20);
+            this.label8.Size = new System.Drawing.Size(87, 29);
             this.label8.TabIndex = 13;
             this.label8.Text = "Quality";
             // 
-            // textBox6
+            // textBoxFuncs
             // 
-            this.textBox6.Location = new System.Drawing.Point(11, 301);
-            this.textBox6.Multiline = true;
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(252, 217);
-            this.textBox6.TabIndex = 19;
+            this.textBoxFuncs.Location = new System.Drawing.Point(16, 379);
+            this.textBoxFuncs.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBoxFuncs.Multiline = true;
+            this.textBoxFuncs.Name = "textBoxFuncs";
+            this.textBoxFuncs.Size = new System.Drawing.Size(376, 332);
+            this.textBoxFuncs.TabIndex = 19;
+            this.textBoxFuncs.Text = "1,0,1\r\n0.6,0.5,0.3,0.4,0.6,-0.1\r\n1,1,2\r\n0.3,0.3,0.5,0.4,0.2,0.5\r\n";
             // 
             // buttonGenerate
             // 
-            this.buttonGenerate.Location = new System.Drawing.Point(11, 540);
+            this.buttonGenerate.Location = new System.Drawing.Point(22, 721);
+            this.buttonGenerate.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.buttonGenerate.Name = "buttonGenerate";
-            this.buttonGenerate.Size = new System.Drawing.Size(247, 64);
+            this.buttonGenerate.Size = new System.Drawing.Size(370, 98);
             this.buttonGenerate.TabIndex = 20;
             this.buttonGenerate.Text = "Generate";
             this.buttonGenerate.UseVisualStyleBackColor = true;
+            this.buttonGenerate.Click += new System.EventHandler(this.buttonGenerate_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(13, 287);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(286, 87);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "Flames:\r\nweight,color[0,1],variation\r\n6x coeffs divided by \',\'";
+            // 
+            // textBoxFlames
+            // 
+            this.textBoxFlames.Location = new System.Drawing.Point(297, 287);
+            this.textBoxFlames.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBoxFlames.Name = "textBoxFlames";
+            this.textBoxFlames.Size = new System.Drawing.Size(106, 26);
+            this.textBoxFlames.TabIndex = 23;
+            this.textBoxFlames.Text = "2";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.ClientSize = new System.Drawing.Size(1213, 634);
+            this.ClientSize = new System.Drawing.Size(1820, 975);
+            this.Controls.Add(this.textBoxFlames);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.buttonGenerate);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.textBox13);
+            this.Controls.Add(this.textBoxFuncs);
+            this.Controls.Add(this.textBoxPalette);
             this.Controls.Add(this.label7);
-            this.Controls.Add(this.textBox15);
+            this.Controls.Add(this.textBoxQuality);
             this.Controls.Add(this.label8);
-            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBoxOversampling);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBoxScale);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBoxCenterY);
+            this.Controls.Add(this.textBoxCenterX);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBoxHeight);
+            this.Controls.Add(this.textBoxWidth);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -227,21 +283,23 @@
 
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBoxWidth;
+        private System.Windows.Forms.TextBox textBoxHeight;
+        private System.Windows.Forms.TextBox textBoxCenterY;
+        private System.Windows.Forms.TextBox textBoxCenterX;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBoxOversampling;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBoxScale;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox13;
+        private System.Windows.Forms.TextBox textBoxPalette;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.TextBox textBox15;
+        private System.Windows.Forms.TextBox textBoxQuality;
         private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBoxFuncs;
         private System.Windows.Forms.Button buttonGenerate;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxFlames;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,73 @@ namespace flam2
         {
             InitializeComponent();
         }
+
+        static Type[] variations = {
+            typeof(Spherical),typeof(Swirl),typeof(Linear),typeof(Sinusoidal)
+        };
+
+        private void buttonGenerate_Click(object sender, EventArgs e)
+        {
+            Flame flame = new Flame();
+
+            flame.width = int.Parse(textBoxWidth.Text);
+            flame.height = int.Parse(textBoxHeight.Text);
+            flame.centerX = float.Parse(textBoxCenterX.Text);
+            flame.centerY = float.Parse(textBoxCenterY.Text);
+            flame.scale = int.Parse(textBoxScale.Text);
+            flame.oversample = int.Parse(textBoxOversampling.Text);
+            flame.quality = int.Parse(textBoxQuality.Text);
+            flame.palleteIndex = int.Parse(textBoxPalette.Text);
+
+            int numFuncs = int.Parse(textBoxFlames.Text);
+            flame.funcs = new Function[numFuncs];
+            flame.weights = new float[numFuncs];
+
+            String[] funcVars = textBoxFuncs.Text.Split('\n');
+
+            for (int i = 0; i < numFuncs; i++)
+            {
+                String[] vars = funcVars[i * 2].Split(',');
+                String[] coeffsStr = funcVars[i * 2 + 1].Split(',');
+                float[] coeffs = new float[6];
+                for (int j = 0; j < 6; j++)
+                {
+                    coeffs[j] = float.Parse(coeffsStr[j]);
+                }
+                Variation var = (Variation)Activator.CreateInstance(variations[int.Parse(vars[2])]);
+                flame.weights[i] = float.Parse(vars[0]);
+
+                flame.funcs[i] = new Function(coeffs,var,Palette.GetColor(flame.palleteIndex,float.Parse(vars[1])));
+            }
+            pictureBox1.Image = flame.Run();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
